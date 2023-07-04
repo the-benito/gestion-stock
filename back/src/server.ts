eb710bd7bad013024a9ed41a356f877567e31cb8
Benoit RIGAULT <benoit.rigault@datanumia.com>
2023-07-04 16:12:17 +0100

    dev: added strict mode

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,14 +1,16 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import serveIndex from "serve-index";
 
 const app = express();
 const port = 3000;
 const publicDir = ".";
 
-app.use((req, res, next) => {
+const logger = (req: Request, res: Response, next: NextFunction) => {
   console.log("req: ", req.method, req.url);
   next();
-});
+};
+
+app.use(logger);
 
 app.use(express.static(publicDir));
 app.use(serveIndex(publicDir, { icons: true }));
