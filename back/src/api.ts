972602f0e8257f0cd996538d23c894578168426e
Benoit RIGAULT <benoit.rigault@datanumia.com>
2023-07-04 16:35:46 +0100

    dev: add stock articles

--- back/src/api.ts
+++ back/src/api.ts
@@ -2,4 +2,13 @@ import express from "express";
 
 const app = express.Router();
 
+const articles = [
+  { id: "a1", name: "Tournevis", price: 2.99, qty: 120 },
+  { id: "a2", name: "Marteau", price: 4.99, qty: 40 },
+];
+
+app.get("/articles", (req, resp) => {
+  resp.json(articles);
+});
+
 export const api = app;
