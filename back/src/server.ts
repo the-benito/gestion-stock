fffd57ea7f72ca68c3101ef2cfb5f0515b7442e9
Benoit RIGAULT <benoit.rigault@datanumia.com>
2023-07-04 15:34:15 +0100

    dev: change require to import

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,5 +1,6 @@
-const express = require("express");
-const serveIndex = require("serve-index");
+import express from "express";
+import serveIndex from "serve-index";
+
 const app = express();
 const port = 3000;
 const publicDir = ".";
