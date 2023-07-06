a53c26ef5c6d40f0dfa0bdb1d861618b6679d39d
Benoit RIGAULT <benoit.rigault@datanumia.com>
2023-07-06 12:46:14 +0100

    dev: use BehaviorSubject

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,24 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Article } from '../interfaces/article';
+import { BehaviorSubject } from 'rxjs';
+
+const articles: Article[] = [
+  {
+    id: 'a1',
+    name: 'Tournevis',
+    price: 2.99,
+    qty: 120,
+  },
+  {
+    id: 'a2',
+    name: 'Marteau',
+    price: 4.99,
+    qty: 40,
+  },
+];
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
-    {
-      id: 'a1',
-      name: 'Tournevis',
-      price: 2.99,
-      qty: 120,
-    },
-    {
-      id: 'a2',
-      name: 'Marteau',
-      price: 4.99,
-      qty: 40,
-    },
-  ];
+  articles$ = new BehaviorSubject<Article[]>(articles);
 
   constructor() {}
 }
