e17f2af05d3c2ac74e0ffbad990ef03cee7ee955
Benoit RIGAULT <benoit.rigault@datanumia.com>
2023-07-07 08:52:36 +0100

    dev: added CORS

--- back/src/api.ts
+++ back/src/api.ts
@@ -8,6 +8,11 @@ const articles: Article[] = [
   { id: "a2", name: "Marteau", price: 4.99, qty: 40 },
 ];
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.get("/articles", (req, resp) => {
   resp.json(articles);
 });
